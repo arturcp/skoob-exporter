4a930fcd4749c65cea69e6616378b811460a662d
Artur <artur.prado@gmail.com>
2020-10-25 10:36:32 -0300

    Add slack notification

--- app/controllers/crawlers_controller.rb
+++ app/controllers/crawlers_controller.rb
@@ -11,6 +11,7 @@ class CrawlersController < ApplicationController
     if user.skoob_user_id > 0
       user.update(import_status: 1)
       SkoobImporterWorker.perform_async(user.skoob_user_id)
+      send_slack_notification(user.skoob_user_id)
 
       redirect_to crawler_path(user.skoob_user_id)
     else
@@ -21,5 +22,12 @@ class CrawlersController < ApplicationController
 
   def show
     @user = SkoobUser.find_by(skoob_user_id: params[:id])
+  end
+
+  private
+
+  def send_slack_notification(skoob_user_id)
+    message = "User #{skoob_user_id} is importing books right now"
+    Slack::Message.send(message, notify_channel: true)
   end
 end
---
+++ app/models/slack/message.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Slack
+  class Message
+    def self.send(message, notify_channel: false, url: nil)
+      webhook = ENV['SLACK_WEBHOOK']
+      return unless webhook
+
+      notifier = Slack::Notifier.new(webhook)
+      message = "<!channel> #{message}" if notify_channel
+      message = "[#{message}](#{url})" if url
+      notifier.ping(message)
+    end
+  end
+end
