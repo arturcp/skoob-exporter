08288c60f81436c680e2d04b6b07a64a5b5fe6ce
Artur Caliendo Prado <artur.prado@gmail.com>
2021-02-21 12:55:22 -0300

    Update sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,8 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDISTOGO_URL'] }
 end
+
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDISTOGO_URL'] }
+end
