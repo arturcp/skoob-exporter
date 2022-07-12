a475d147c5e7ec807ca120ead94a6ea166b214bc
Artur <artur.prado@gmail.com>
2022-07-12 15:42:04 -0300

    Change redis addon

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDIS_URL"])
 REDIS = Redis.new(:url => uri)
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISTOGO_URL'] }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDISTOGO_URL'] }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
