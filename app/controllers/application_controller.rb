07c3ff999a1bac33d55719d46f78d4ae949b6062
Artur Prado <artur.prado@gmail.com>
2023-06-18 10:45:12 -0300

    Remove ssl enforcement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :force_http
+
+  private
+
+  def force_http
+    if request.ssl?
+      response.headers['Strict-Transport-Security'] = 'max-age=0; includeSubDomains'
+      redirect_to protocol: 'http://', status: :moved_permanently
+    end
+  end
 end
