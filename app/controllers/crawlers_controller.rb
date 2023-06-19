656bc5fd168027d9ab4403673dde31d6fe544c1d
Artur Prado <artur.prado@gmail.com>
2023-06-19 12:12:24 -0300

    Remove redirect rule

--- app/controllers/crawlers_controller.rb
+++ app/controllers/crawlers_controller.rb
@@ -1,10 +1,4 @@
 class CrawlersController < ApplicationController
-  def index
-    if ENV['PRODUCTION_URL'].present?
-      redirect_to ENV['PRODUCTION_URL'], status: 301
-    end
-  end
-
   def create
     user = SkoobUser.login(params[:email], params[:password])
 
