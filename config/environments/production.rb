67aff960f96704a7224022d006fb1ab16c570c0b
Artur Prado <artur.prado@gmail.com>
2023-06-19 13:52:18 -0300

    Disable protection agains forgery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
+  config.action_controller.default_protect_from_forgery = false
 end
