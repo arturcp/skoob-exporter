06e6ced3aca1259964560830eb3fefad0afa5ed7
Artur Prado <artur.prado@gmail.com>
2023-07-14 16:24:29 -0300

    Show loading on generate button

--- app/javascript/lib/login.js
+++ app/javascript/lib/login.js
@@ -2,11 +2,17 @@ export default class Login {
   constructor() {
     const form = document.getElementById('crawler-form');
     const submitButton = document.getElementById('crawler-submit');
+    const loadingInfo = document.querySelector('.loading-info');
+    const alert = document.querySelector('.login-alert');
 
     if (form) {
       form.addEventListener('submit', () => {
         submitButton.disabled = true;
-        submitButton.value = 'Aguarde...';
+        if (alert) {
+          alert.style.display = 'none';
+        }
+        submitButton.innerHTML = '<img class="book-loading" src="/book-loading.gif" /> Aguarde...';
+        loadingInfo.style.display = 'block';
       });
     }
   }
